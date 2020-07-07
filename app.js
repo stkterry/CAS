276466751a542d049d709c728ca50d5109cee689
Steven K. Terry <stkterry@gmail.com>
2020-07-07 15:44:33 -0400

    Add initial users api/route

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const mongoose = require("mongoose");
 
 const app = express();
 const db = require("./config/keys").mongoURI;
+const port = process.env.PORT || 5001;
+const users = require("./routes/api/users");
+
+
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-
-
+  
 app.get("/", (req, res) => res.send("Hello World!!!"));
-
-
-const port = process.env.PORT || 5000;
+app.use("/api/users", users);  
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
