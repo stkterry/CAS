3e4f780242c69c34f93b1f590e377d998db3259a
Steven K. Terry <stkterry@gmail.com>
2020-07-15 23:33:37 -0400

    Add initial seeder functionality, add static_seeds, add 'addGame, removeGame' static functions to User model

--- server/models/User.js
+++ server/models/User.js
@@ -22,6 +22,22 @@ const UserSchema = new Schema({
     type: Date,
     default: Date.now
   },
-})
+});
+
+UserSchema.statics.addGame = function (user_id, game_id) {
+  return this.findByIdAndUpdate(
+    user_id,
+    { $addToSet: { game_ids: game_id }},
+    { new: true }
+  )
+};
+
+UserSchema.statics.removeGame = function (user_id, game_id) {
+  return this.findByIdAndUpdate(
+    user_id,
+    { $pull: { game_ids: game_id }},
+    { new: true }
+  )
+};
 
 module.exports = User = mongoose.model("User", UserSchema);
