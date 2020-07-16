e9f7409924ef19cfa5df9685fbcdfeb253559f4c
Steven K. Terry <stkterry@gmail.com>
2020-07-15 22:16:38 -0400

    Migrate server files and folders to /server, change frontend folder and related scripts to client, move documentation to /documentation

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://StevenKT:nCo4P9nqlStCfJfM@cah.bo7wx.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  secretOrKey: "@*a7+tzV9^':d27roNH[[}I-)LJI.u"
+}
