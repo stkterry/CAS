993f65deb388a1458220d9b169b065fcaf73a219
Steven K. Terry <stkterry@gmail.com>
2020-07-25 17:56:11 -0400

    Add to messaging components, update to styling

--- client/src/components/messages/MessageBox.jsx
+++ client/src/components/messages/MessageBox.jsx
@@ -5,19 +5,19 @@ class MessageBox extends React.Component {
 
 
 
-
-
-
-
-
-
   render = () => {
 
     return (
       <div id="message_box-container">
-        <div id="message_box">
-          
+      <div id="message_box">
+        <div id="message_box-left">
+          <div id="message_box-info"></div>
+          <div id="message_box-users"></div>
         </div>
+        <div id="message_box-right">
+          <div id="message_box-body"></div>
+        </div>
+      </div>
       </div>
     )
   }
---
+++ client/src/components/messages/message_box_container.js
@@ -0,0 +1,14 @@
+import { connect } from "react-redux";
+// import { getGames } from "../../actions/game_actions";
+
+import MessageBox from "./MessageBox";
+
+const mSP = state => ({
+
+});
+
+
+const mDP = dispatch => ({
+})
+
+export default connect(mSP, mDP)(MessageBox);
